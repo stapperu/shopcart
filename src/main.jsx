2bcd547efe9136bd62904ae01f60c8d8c8b36093
stapperu <k.kubiak94@gmail.com>
2026-01-15 19:01:10 +0100

    retry

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { ProductProvider } from  "./contexts/ProductContext.jsx";
-import { CartProvider } from "./contexts/CartContext.jsx";
+import { ProductProvider } from  "./contexts/ProductContext";
+import { CartProvider } from "./contexts/CartContext";
 import App from "./App.jsx";
 
 
