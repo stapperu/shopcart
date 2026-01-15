18c57510e4b91233b5adb96924f4730b12ea4968
stapperu <k.kubiak94@gmail.com>
2026-01-15 19:23:41 +0100

    hopefully fixed windows sabotage with filename casings

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { CartProvider } from "temp.jsx";
+import { CartProvider } from "./contexts/CartContext.jsx";
 import { ProductProvider } from  "./contexts/ProductContext.jsx";
 import App from "./App.jsx";
 
