4dfc37d50cb7b5990eaafc6642b24f68dd7e5034
stapperu <k.kubiak94@gmail.com>
2026-01-15 18:54:53 +0100

    Update main.jsx
    trying to fix deployment issues, almost certainly caused by casing/caching/whatever error - moving things around making no typo is present

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { CartProvider } from "./contexts/CartContext.jsx";
+import { ProductProvider } from  "./contexts/ProductContext.jsx";
 import App from "./App.jsx";
-import { ProductProvider } from  "./contexts/ProductContext.jsx"
 
-import {CartProvider} from "./contexts/CartContext.jsx";
 
 
 createRoot(document.getElementById("root")).render(
