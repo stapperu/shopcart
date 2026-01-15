00035e399a3ea5093f6b56e11fe1840d03514453
stapperu <k.kubiak94@gmail.com>
2026-01-15 18:59:10 +0100

    this is mad, the names are exactly the same yet GHactions fail to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { CartProvider } from "./contexts/CartContext.jsx";
 import { ProductProvider } from  "./contexts/ProductContext.jsx";
+import { CartProvider } from "./contexts/CartContext.jsx";
 import App from "./App.jsx";
 
 
