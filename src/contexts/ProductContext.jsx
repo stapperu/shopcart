3870e753f2c461507acb5c7b13fa6199198319f0
stapperu <k.kubiak94@gmail.com>
2026-01-15 20:17:29 +0100

    path partially in env file

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-
 export const ProductContext = createContext();
 
 export function ProductProvider({ children }) {
@@ -10,7 +9,7 @@ export function ProductProvider({ children }) {
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const res = await fetch(`/data/db.json`);
+				const res = await fetch(`${import.meta.env.VITE_BASE_URL}/data/db.json`);
 				if (!res.ok) {
 					throw new Error("Failed to fetch data");
 				}
