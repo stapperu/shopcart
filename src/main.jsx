9f52ff95dceb3698fe89151ab23cfa15bdf81288
stapperu <k.kubiak94@gmail.com>
2026-01-15 18:47:41 +0100

    imports are not being properly updated sometimes, keep throwing errors until you move it to another line(at least last time it worked so)

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ProductProvider } from  "./contexts/ProductContext.jsx"
-import { CartProvider } from "./contexts/CartContext.jsx";
+
+import {CartProvider} from "./contexts/CartContext.jsx";
+
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
